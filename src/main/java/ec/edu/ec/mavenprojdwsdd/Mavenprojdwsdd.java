def19c9b9cbc541a4d0fcecb0cfef157cb5f715c
JIrleidySantos <jirleidysantosm@gmail.com>
2024-11-27 22:04:11 -0500

    Primer

---
+++ src/main/java/ec/edu/ec/mavenprojdwsdd/Mavenprojdwsdd.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ec.mavenprojdwsdd;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Mavenprojdwsdd {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
